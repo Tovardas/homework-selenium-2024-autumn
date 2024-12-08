c22edd7b2d9c15225bc253b875151a1a418447f1
DeadBread-001 <nikita10062004m@gmail.com>
2024-12-08 16:06:06 +0300

    еще удалил ненужные тесты

--- hw/code/ui/verification/settings_access_page_verification.py
+++
@@ -1,7 +0,0 @@
-class SettingsAccessPageVerification:
-    def __init__(self, settings_access_page):
-        self.settings_access_page = settings_access_page
-
-    def check_header_and_sign_visibility(self):
-        assert self.settings_access_page.header_and_sign_became_visible()
-        assert self.settings_access_page.add_button_became_visible()
