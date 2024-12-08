6eec1768d13083b6a41484f8b9e54cf2d6d6aad0
DeadBread-001 <nikita10062004m@gmail.com>
2024-12-08 16:02:07 +0300

    удалил ненужные тесты

--- hw/code/ui/verification/footer_verification.py
+++
@@ -1,9 +0,0 @@
-class FooterVerification:
-    def __init__(self, main_page):
-        self.main_page = main_page
-
-    def check_url_opened(self, expected_url):
-        assert self.main_page.driver.current_url == expected_url
-
-    def check_selected_language(self, expected_language):
-        assert self.main_page.get_selected_language_from_footer() == expected_language
--- hw/code/ui/verification/forum_page_verification.py
+++
@@ -1,9 +0,0 @@
-class ForumPageVerification:
-    def __init__(self, forum_page):
-        self.forum_page = forum_page
-
-    def check_page_title(self, expected_title):
-        assert self.forum_page.get_page_title() == expected_title
-
-    def check_url_opened(self, expected_url):
-        assert self.forum_page.driver.current_url == expected_url
